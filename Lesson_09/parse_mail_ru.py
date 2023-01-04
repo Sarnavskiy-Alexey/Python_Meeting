ce72ec07c65696a38d6be376cfdc9f5b1e7b4e10
Sarnavskiy-Alexey <93035243+Sarnavskiy-Alexey@users.noreply.github.com>
2023-01-04 13:36:46 +0300

    Added two new methods to get one tiding by href.

--- Lesson_09/parse_mail_ru.py
+++ Lesson_09/parse_mail_ru.py
@@ -22,9 +22,27 @@ def import_main_news_from_mail_ru() -> ResultSet[PageElement]:
     return news
 
 
+def import_news_from_news_mail_ru(href: str) -> PageElement:
+    url = href
+    response = requests.get(url).text
+    soup = bs(response, 'html.parser')
+    return soup.find('div', class_='article__intro meta-speakable-intro')
+
+
+def read_one_piece_of_news(n: int, hrefs_list: list[str]) -> list[str]:
+    href = hrefs_list[n - 1]
+    tidings = import_news_from_news_mail_ru(href)
+    return [tidings.text] + [href]
+
+
 if __name__ == '__main__':
     print('Главные новости этого часа на mail.ru:')
     news = import_main_news_from_mail_ru()
     hrefs = find_all_hrefs(news)
     for i, a in enumerate(zip(news, hrefs), start=1):
         print('\t', i, '. ', a[0].text, ' - ', a[1], sep='')
+    number = int(input('\nВведите номер новости, которую хотите открыть: ')) % 10
+    if number:
+        print(' - '.join(read_one_piece_of_news(number, hrefs)))
+    else:
+        print('Номер новости не найден')
